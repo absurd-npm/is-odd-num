5b736fcde81bfe13de28ad43b4bced51e4545ec5
Prasundas99 <prasundas115@gmail.com>
2021-08-21 23:50:35 +0530

    [NEW] Test

---
+++ test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('mocha');
+const assert = require('assert');
+const isOdd = require('./');
+
+describe('isOdd', function() {
+  it('should return true if the number is odd:', function() {
+    assert(!isOdd(0));
+    assert(!isOdd(2));
+    assert(isOdd(1));
+    assert(isOdd(-1));
+    assert(isOdd(-3));
+    assert(isOdd(1.0e0));
+    assert(isOdd(9007199254740991));
+  });
+
+  it('should work with strings:', function() {
+    assert(!isOdd('0'));
+    assert(!isOdd('2'));
+    assert(isOdd('1'));
+    assert(isOdd('3'));
+    assert(isOdd('9007199254740991'));
+  });
+
+  it('should throw an error when an invalid value is passed', function() {
+    assert.throws(() => isOdd(), /expected a number/);
+    assert.throws(() => isOdd('foo'), /expected a number/);
+    assert.throws(() => isOdd('1.1e0'), /expected an integer/);
+    assert.throws(() => isOdd('9007199254740992'), /value exceeds maximum safe integer/);
+    assert.throws(() => isOdd(9007199254740992), /value exceeds maximum safe integer/);
+  });
+});
